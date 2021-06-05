a9e173f75f858c5c7dea8cb09930996607e943d1
Kaspars Siricenko <kaspars.siricenko.ext@konecranes.com>
2021-06-05 15:13:31 +0300

    fixed hp and added player prop

--- public/main.js
+++ public/main.js
@@ -5,14 +5,16 @@ let socket = io()
 
 const player1 = new Player({
   name: 'SCORPION',
-  hp: 14,
+  player: 1,
+  hp: 100,
   img: './assets/scorpion.gif',
   weapon: ['vodka'],
 })
 
 const player2 = new Player({
   name: 'KITANA',
-  hp: 88,
+  player: 2,
+  hp: 100,
   img: './assets/kitana.gif',
   weapon: ['whiskey'],
 })
