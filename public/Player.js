4b420060584f736aa6426207685501fb177c3185
Kaspars Siricenko <kaspars.siricenko.ext@konecranes.com>
2021-06-07 12:26:34 +0300

    added new hp methods for player

--- public/Player.js
+++ public/Player.js
@@ -9,6 +9,20 @@ class Player {
   attack() {
     console.log(`${this.name} Fight...`)
   }
+  renderHP(hit) {
+    const newHp = player.hp - hit
+    const lost = newHp <= 0
+    player.hp = lost ? 0 : newHp
+    return lost
+  }
+  elHP() {
+    return document.querySelector(
+      `.player${player.player} > .progressbar > .life`
+    )
+  }
+  renderHP() {
+    this.elHP().style.width = `${this.hp}%`
+  }
 }
 
 export default Player
