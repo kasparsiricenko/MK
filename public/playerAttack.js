50a9d194a4f9f1e17b034a10f3f471e4650fcace
Kaspars Siricenko <kaspars.siricenko.ext@konecranes.com>
2021-06-18 01:59:17 +0300

    updated player attack

--- public/playerAttack.js
+++ public/playerAttack.js
@@ -1,16 +1,12 @@
-import { HIT } from './config.js'
-import { getRandomizedHit } from './utils.js'
-
 const playerAttack = () => {
   const $hits = Array.from(document.getElementsByName('hit'))
   const $defences = Array.from(document.getElementsByName('defence'))
-  const $hit = $hits.find(($hit) => $hit.checked)
-  const hit = $hit.value
-  const value = getRandomizedHit(HIT[hit])
+  const $attack = $hits.find(($hit) => $hit.checked)
+  const attack = $attack.value
   const $defence = $defences.find(($defence) => $defence.checked)
   const defence = $defence.value
 
-  return { hit, value, defence }
+  return { attack, defence }
 }
 
 export default playerAttack
