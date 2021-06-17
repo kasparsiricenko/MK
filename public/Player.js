f481a6b1ee21cef7c13171ab0d5719f94a4996f0
Kaspars Siricenko <kaspars.siricenko.ext@konecranes.com>
2021-06-18 01:59:05 +0300

    updated player js

--- public/Player.js
+++ public/Player.js
@@ -1,26 +1,57 @@
 class Player {
-  constructor(props) {
+  constructor({ render = true, ...props } = {}) {
     this.name = props.name
     this.nameUpperCase = props.name.toUpperCase()
-    this.player = props.player
-    this.hp = props.hp
-    this.img = props.img
+    this.position = props.position
+    this.hp = props.hp ?? 100
+    this.character = props.character
+    this.img = props.img ?? `./assets/${this.character}.gif`
     this.weapon = props.weapon
+    render && this.render()
   }
   attack() {
     console.log(`${this.name} Fight...`)
   }
-  changeHP(hit) {
-    const newHp = this.hp - hit
-    this.hp = newHp <= 0 ? 0 : newHp
+  setHp(newHp) {
+    this.hp = newHp
+    this.renderHp()
   }
-  elHP() {
-    return document.querySelector(
-      `.player${this.player} > .progressbar > .life`
-    )
+  renderHp() {
+    this.$hp.style.width = `${this.hp}%`
   }
-  renderHP() {
-    this.elHP().style.width = `${this.hp}%`
+  render() {
+    const $player = document.createElement('div')
+    $player.classList.add(`player${this.position}`)
+
+    const $progressBar = document.createElement('div')
+    $progressBar.classList.add('progressbar')
+
+    const $character = document.createElement('div')
+    $character.classList.add('character')
+
+    $player.append($progressBar, $character)
+
+    const $life = document.createElement('div')
+    $life.classList.add('life')
+
+    const $name = document.createElement('div')
+    $name.classList.add('name')
+
+    $progressBar.append($life, $name)
+
+    const $image = new Image()
+    $image.src = this.img
+
+    $character.append($image)
+
+    $life.style.width = `${this.hp}%`
+    $name.append(this.name)
+
+    this.$hp = $life
+
+    const $arenas = document.getElementsByClassName('arenas')[0]
+
+    $arenas.append($player)
   }
 }
 
