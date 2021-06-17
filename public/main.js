e980f1a20ee1d301e0f6f1a97d24af4479e4bdd6
Kaspars Siricenko <kaspars.siricenko.ext@konecranes.com>
2021-06-17 22:55:23 +0300

    updated main.js

--- public/main.js
+++ public/main.js
@@ -1,27 +1,5 @@
-import createPlayer from './createPlayer.js'
-import Player from './Player.js'
-import addOnSubmitAttack from './addOnSubmitAttack.js'
 import Game from './Game.js'
 
-// const player1 = new Player({
-//   name: 'scorpion',
-//   player: 1,
-//   hp: 100,
-//   img: './assets/scorpion.gif',
-//   weapon: ['vodka'],
-// })
-
-// const player2 = new Player({
-//   name: 'kitana',
-//   player: 2,
-//   hp: 100,
-//   img: './assets/kitana.gif',
-//   weapon: ['whiskey'],
-// })
-
-// createPlayer(player1)
-// createPlayer(player2)
-
-// addOnSubmitAttack(player1, player2)
-
 const game = new Game()
+
+game.start()
